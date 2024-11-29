69f8f65f58ee1185a7ebb6bc3870fc83063b04eb
Rohit Balage <thatcomputerboy26@gmail.com>
2024-11-29 05:43:21 -0500

    added reducer conventional method state and dispatch in counterScreen and SquareScreen

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,25 +1,42 @@
-import React, {useState}  from "react";
+import React, {useReducer}  from "react";
 import {View, Text, StyleSheet, Button} from 'react-native';
+
+
+const reducer = (state, action) =>
+{
+    //state === {count: number}
+    // action== {type: 'increment' || 'decrement', payload: 1 or -1}
+
+    switch(action.type)
+    {
+        case 'increment':
+        return { ...state, count: state.count + action.payload};
+        case 'decrement':
+            return {...state, count: state.count -action.payload}
+            default:
+                return state;
+    }
+    
+
+}
+
 
 const CounterScreen = () =>
 {
-const [counter, setCounter] = useState(0);
+const [state, dispatch] = useReducer(reducer, {count:0});
 
     return (<View>
-        <Button title="Increase" onPress={() => {
-            // don't do this
-            // counter++
-            setCounter(counter+1);
+        <Button title="Increase" 
+        onPress={() => {
+       dispatch ({type: 'increment', payload :1})
         }}>  </Button>
 
         <Button title="Decrease" onPress={() => {
-            // don't do this
-            // counter--
-            setCounter(counter -1);
+         dispatch ({type: 'decrement', payload :1})
         }}>  </Button>
 
         <Text>
-            Current Count : {counter}
+            Current Count : {state.count}
         </Text>
     </View>
 );
